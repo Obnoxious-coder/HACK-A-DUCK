67a9a840d27ff1e7e62c141fa1005f273dbd04a5
Kunal Juneja <47492588+cashewit@users.noreply.github.com>
2020-11-04 22:26:39 +0530

    admin functions

---
+++ admin.py
@@ -0,0 +1,36 @@
+from flask import request, jsonify
+from app import mongo
+
+
+def admin():
+    try:
+        question = request.get_json()
+        mongo.db.questions.insert({
+            "text": question['text'],
+            "category": question['category'],
+            "type": question['type'],
+            "difficulty": question['difficulty'],
+            "answer": question['answer']
+        })
+        return jsonify({'message': 'success'}), 200
+
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'error'}), 500
+
+
+def list_category():
+    try:
+        if request.method == 'POST':
+            category = request.get_json()['category']
+            mongo.db.category.insert({"name": category})
+            return jsonify({'message': 'success'}), 200
+
+        else:
+            category = mongo.db.category.find({})
+            result = [element['name'] for element in category]
+            return jsonify(result), 200
+
+    except Exception as e:
+        print(e)
+        return jsonify({'message': 'error'}), 500
