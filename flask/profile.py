2aa254f5e4cf8ca7d950856804f60906452041a9
cashewit <cashewit@github.com>
2020-11-05 19:42:18 +0530

    Profile page updated!

--- flask/profile.py
+++ flask/profile.py
@@ -1,10 +1,11 @@
-from flask import jsonify, request
+from flask import jsonify, request, session
 from app import  mongo
 import operator
 
 def profile():
 	try:
-		user = mongo.db.users.find_one({'':''}) # TODO: Add identification criteria
+		email = session['user']['email']
+		user = mongo.db.users.find_one({'email':email})
 		if user:
 			user['_id'] = str(user['_id'])
 			return jsonify(user), 200
@@ -19,9 +20,11 @@ def profile():
 def leader_board():
 	try:
 		data = mongo.db.users.find()
-		result = [{"name": d['name'], "score": d['score']} for d in data]
-		result = [element for element in sorted(result, key=operator.itemgetter("age"), reverse=True)]
-		return jsonify(result), 200
+		if data:
+			result = [{"name": d['name'], "score": d['score']} for d in data]
+			result = [element for element in sorted(result, key=operator.itemgetter("age"), reverse=True)]
+			return jsonify(result), 200
+		return jsonify(list()), 200
 	
 	except Exception as e:
 		print(e)
